5038538d302500e527a29505d1ba11f2405704b1
Clément <clement2104.boillot@gmail.com>
2024-01-11 09:42:39 +0100

    Fix sting_insert

--- string_insert.c
+++ string_insert.c
@@ -5,15 +5,26 @@
 ** string_insert
 */
 
+#include <string.h>
+
 #include "string.h"
 
 void insert_c(string_t *this, size_t pos, const char *str)
 {
+    char *start = NULL;
+    char *end = NULL;
+
     if (this == NULL || str == NULL || this->str == NULL)
         return;
-    if (pos <= (size_t)this->length(this))
-        this->str[pos] = '\0';
+    start = strndup(this->str, pos);
+    end = (pos <= (size_t)this->length(this))
+        ? strdup(this->str + pos)
+        : strdup("");
+    this->assign_c(this, start);
     this->append_c(this, str);
+    this->append_c(this, end);
+    free(start);
+    free(end);
 }
 
 void insert_s(string_t *this, size_t pos, const string_t *str)
